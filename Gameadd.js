74916a6fa1abe28f87b77b29f3c75570850dd82b
Benedict Ojekudo <ojekudobenedict1@gmail.com>
2023-02-08 23:37:35 +0100

    Update Gameadd.js

--- Gameadd.js
+++ Gameadd.js
@@ -1,1 +1,23 @@
+class Gamecall extends React.Component{
+    constructor(props){
+        super(props);
+        console.log(this.props);
+    }
+    render(){
+        if(this.props.games){
+            const gameList = games.map(game => {
+                    return React.createElement('ul', null, 
+                            React.createElement('li', null, game)
+                                    );
+            });
+            return gameList;
+        }
+        return React.createElement('h1', null, 'no games available');
+    }
+}
 
+const games = ['ff9', 'COD'];
+
+const element = React.createElement(Gamecall, {games: games});
+
+ReactDOM.render(element, document.getElementById('root'));
