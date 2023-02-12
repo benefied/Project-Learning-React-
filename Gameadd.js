8f16c29cd081aee30c69c05e0bed3be6232af0d4
Benedict Ojekudo <ojekudobenedict1@gmail.com>
2023-02-12 20:41:54 +0100

    Update Gameadd.js
    I learnt how to use jsx syntax instead of normal react element syntaxes so I converted this first

--- Gameadd.js
+++ Gameadd.js
@@ -1,23 +1,26 @@
-class Gamecall extends React.Component{
-    constructor(props){
-        super(props);
-        console.log(this.props);
-    }
-    render(){
-        if(this.props.games){
-            const gameList = games.map(game => {
-                    return React.createElement('ul', null, 
-                            React.createElement('li', null, game)
-                                    );
-            });
-            return gameList;
-        }
-        return React.createElement('h1', null, 'no games available');
-    }
-}
-
-const games = ['ff9', 'COD'];
-
-const element = React.createElement(Gamecall, {games: games});
-
-ReactDOM.render(element, document.getElementById('root'));
+<script type="text/babel">
+        class Gamecall extends React.Component {
+            constructor(props) {
+              super(props);
+              console.log(this.props);
+            }
+            render() {
+              if (this.props.games) {
+                const gameList = games.map((game) => {
+                   return <ul>
+                      <li>{game}</li>
+                    </ul>
+                });
+          
+                return gameList;
+              }
+              return <h1> no games available </h1>;
+            }
+          }
+          
+          const games = ["ff9", "COD"];
+          
+          const element = <Gamecall games={games} />;
+          
+          ReactDOM.render(element, document.getElementById("root"));
+    </script>
